5ac6c20bf2e1e07c41dcf416610bb69503bad44d
Eric Lopes Oliveira <65664969+olvericc@users.noreply.github.com>
2023-03-10 21:38:39 -0300

    problem example v2

--- problemExample01/src/application/Program.java
+++ problemExample01/src/application/Program.java
@@ -8,31 +8,47 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		final String Error = "Out of interval";
+		
 		double firstValue, secondValue,thirdValue, avg;
 	
 		System.out.print("Please type the first value: ");
 		firstValue = sc.nextDouble();
+		// testing the first value
 		if (firstValue >= 0 && firstValue <= 10) {
 			System.out.print("Type the second value: ");
 			secondValue = sc.nextDouble();
+			
+			// testing the second value
 			if (secondValue >= 0 && secondValue <= 10) {
 				System.out.print("Type the third value: ");
 				thirdValue = sc.nextDouble();
+				
+				// testing the third value
 				if (thirdValue >= 0 && thirdValue <= 10) {
 					avg = (firstValue + secondValue + thirdValue) / 3;
+					
+					// testing the average -> approved or not approved
+					if (avg >= 6) {
+						System.out.println("approved");
+					}
+					else {
+						System.out.println("not approved");
+					}
 					System.out.println("Your average is: " + avg);
-				} else {
-					System.out.print("out of interval");
+				} // if 3
+				else {
+					System.out.println(Error);
 				}
-			}
+			} // if 2
 			else {
-				System.out.print("out of interval");
+				System.out.println(Error);
 			}
-		} 
+		} // if 1
 		else {
-			System.out.print("out of interval");
+			System.out.println(Error);
 		}
-		System.out.println("Try again!");
+
 		sc.close();
 
 	}
