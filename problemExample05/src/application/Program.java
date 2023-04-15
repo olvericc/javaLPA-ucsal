133c5582d2833744174230cc4ec5fb600a4195a7
Eric Lopes Oliveira <65664969+olvericc@users.noreply.github.com>
2023-04-14 21:09:35 -0300

    loop structure - while

---
+++ problemExample05/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int number = 5;
+		
+		while (number < 50) {
+			
+				System.out.print(number + " ");
+			
+			number += 3;
+			
+		}
+	}
+}
